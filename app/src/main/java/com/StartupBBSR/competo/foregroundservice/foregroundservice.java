6c2dc2fce2d89e50c4ef1bec9294162c7cb51711
Aditya Singh <68631004+adisingh925@users.noreply.github.com>
2021-10-02 14:01:51 +0530

    ui finalised

--- app/src/main/java/com/StartupBBSR/competo/foregroundservice/foregroundservice.java
+++ app/src/main/java/com/StartupBBSR/competo/foregroundservice/foregroundservice.java
@@ -8,12 +8,18 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Build;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
 import com.StartupBBSR.competo.Activity.MainActivity;
 import com.StartupBBSR.competo.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class foregroundservice extends Service {
 
@@ -26,11 +32,27 @@ public class foregroundservice extends Service {
         return null;
     }
 
-    private void notificationchannel()
+    @Override
+    public void onCreate() {
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+        db.collection("messagenumber").document(firebaseAuth.getUid()).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                Log.d("Data recieved", String.valueOf(value.getData()));
+                notificationchannel(String.valueOf(value.getData()));
+            }
+        });
+
+    }
+
+    private void notificationchannel(String data)
     {
         if(Build.VERSION.SDK_INT>=26)
         {
-            NotificationChannel notificationchannel = new NotificationChannel(channel_id,channel_name, NotificationManager.IMPORTANCE_HIGH);
+            NotificationChannel notificationchannel = new NotificationChannel(channel_id,channel_name, NotificationManager.IMPORTANCE_NONE);
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(notificationchannel);
         }
@@ -38,7 +60,7 @@ public class foregroundservice extends Service {
         PendingIntent pendingintent = PendingIntent.getActivities(this,0,new Intent[]{intent},0);
         Notification notification = new NotificationCompat.Builder(this,channel_id)
                 .setContentTitle("Foreground Service")
-                .setContentText("Service is running in the background")
+                .setContentText(data)
                 .setSmallIcon(R.drawable.ic_settings)
                 .setContentIntent(pendingintent)
                 .build();
@@ -48,7 +70,8 @@ public class foregroundservice extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        notificationchannel();
+
+        notificationchannel("hi");
 
         return START_STICKY;
     }
