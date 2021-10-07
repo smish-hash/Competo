235abc0b85fa70c909fff5e2bea5d4fd0a10b29b
Aditya Singh <68631004+adisingh925@users.noreply.github.com>
2021-10-07 16:57:14 +0530

    event and message request notification

--- app/src/main/java/com/StartupBBSR/competo/jobscheduler/jobscheduler.java
+++ app/src/main/java/com/StartupBBSR/competo/jobscheduler/jobscheduler.java
@@ -13,17 +13,22 @@ import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
 import com.StartupBBSR.competo.Activity.MainActivity;
 import com.StartupBBSR.competo.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.io.IOException;
@@ -103,8 +108,7 @@ public class jobscheduler extends JobService {
 
             ////////////////////////////////////////////////////////////////////////////////////////
 
-            db.collection("Chats").document(firebaseAuth.getUid()).collection("Messages").addSnapshotListener((value, error) -> {
-
+            db.collectionGroup("Messages").addSnapshotListener((value, error) -> {
                 for(DocumentChange dc : value.getDocumentChanges())
                 {
                     if (dc.getType() == DocumentChange.Type.ADDED) {
