2a9693abeed171d3d98c0267a58c08db9206e7b9
aditya singh <adisingh925@gmail.com>
2022-01-30 20:10:55 +0530

    team chat done

--- app/src/main/java/com/StartupBBSR/competo/Activity/TeamChatDetailActivity.java
+++ app/src/main/java/com/StartupBBSR/competo/Activity/TeamChatDetailActivity.java
@@ -202,7 +202,7 @@ public class TeamChatDetailActivity extends AppCompatActivity implements AddTeam
                                                     DocumentSnapshot document = task.getResult();
                                                     if (document.exists()) {
                                                         Log.d("data", "DocumentSnapshot data: " + document.getString("token"));
-                                                        sendfcm(document.getString("token"));
+                                                        sendfcm(document.getString("token"),message,senderName);
                                                     } else {
                                                         Log.d("data", "No such document");
                                                     }
@@ -510,7 +510,7 @@ public class TeamChatDetailActivity extends AppCompatActivity implements AddTeam
         }
     }
 
-    public void sendfcm(String token)
+    public void sendfcm(String token, String message, String senderName)
     {
         Runnable runnable = () -> {
             OkHttpClient client = new OkHttpClient();
@@ -519,8 +519,9 @@ public class TeamChatDetailActivity extends AppCompatActivity implements AddTeam
                     "    \"data\" : {\n" +
                     "      \"id\" : \""+firebaseAuth.getUid()+"\",\n" +
                     "      \"category\" : \"team\",\n" +
-                    "      \"title\":\"Team\",\n" +
-                    "      \"body\":\"You have a new TEAM message\"\n" +
+                    "      \"teamId\" : \""+teamID+"\",\n" +
+                    "      \"title\":\""+senderName+"\",\n" +
+                    "      \"body\":\""+message+"\"\n" +
                     "    },\n" +
                     "    \"to\":\""+token+"\"\n" +
                     "}");
